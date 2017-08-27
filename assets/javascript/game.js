009ac35bef5aa6fc1335768078d8f78f296c5675
saschaschneider <s4schnei@gmail.com>
2017-08-27 16:41:19 -0700

    V1.1 (Lives work now - still only a  single word, no graphics, no 'Game Over')

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,34 +5,41 @@ var blanks ="";
 var userInput = "";
 var lives = 6; // According to Wikipedia, the user has 6 lives
 
-var i = blanks.length;  // Creates the blanks
-while (i < gamewordLength) {
+var j = blanks.length;  // Creates the blanks
+while (j < gamewordLength) {
     blanks += " _";
-    i++;
+    j++;
    }
 
 var paragraph = document.getElementById("demo").innerHTML = "The word you are looking for has " + gamewordLength + " characters, can you guess it?";
 var paragraph1 = document.getElementById("blanks").innerHTML =  "Word:"+blanks;
+var pLives = document.getElementById("pLives").innerHTML = " Lives: " + lives;
+
 
  document.onkeyup = function(event){
       userInput = event.key;
       userInput = userInput.toUpperCase();
 
       console.log("the userInput is " + userInput) // 
-      // check user input for debugging 
+      var index = gameWord.indexOf(userInput);
+      console.log(index);
 
-    for (var i = 0; i < gamewordLength; i++){
-	if (gameWord.charAt(i) === userInput) {
-		console.log("that works, the right position is" +i);
+
+
+      	if (index >= 0) {
+		console.log("that works, the right position is " +index);
 		blanks = blanks.split('');
-		blanks[i*2+1] = userInput;
+		blanks[index*2+1] = userInput;
 		blanks = blanks.join('');
 		var paragraph1 = document.getElementById("blanks").innerHTML =  "Word:"+blanks;
-	}}}
+	} else {
+		lives = lives -1;
+		console.log("lives = " +lives)
+		var pLives = document.getElementById("pLives").innerHTML = " Lives: " + lives;
 
+}}
 
-	var pLives = document.getElementById("pLives").innerHTML = " Lives: " + lives;
-
+	
 
 
 console.log(paragraph);
